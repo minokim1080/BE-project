05a0b660e8dfbbe2633032e64e68b96ec4263b81
minho <minhokim0806@gmail.com>
2023-01-05 15:42:29 +0900

    Initial commit

---
+++ src/main/java/com/example/mybulletjournal/MyBulletJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybulletjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBulletJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBulletJournalApplication.class, args);
+    }
+
+}
